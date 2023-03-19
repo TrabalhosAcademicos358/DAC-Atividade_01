f164bbb0678491ce1172fc52d0da9e33a6c00e73
Pedro Júnior <pedrojuniorifpb@gmail.com>
2023-03-19 19:32:11 -0300

    Adicionando arquivos de loading

---
+++ frontend/js/loadings.js
@@ -0,0 +1,20 @@
+import {
+    generateCardPublisher,
+    generateCardBook
+} from "./generate.js"
+
+import * as controllerPublish from "./crud/crud_publisher.js"
+import * as controllerBooks from "./crud/crud_books.js"
+
+export const loadingBooks = async () => {
+    const listBooks = await controllerBooks.getAll()
+    for (const book of listBooks) 
+        generateCardBook(book)
+}
+
+export const loadingPublisher = async () => {
+    const listPublishers = await controllerPublish.getAll()
+    console.log(await listPublishers)
+    for (const publisher of listPublishers) 
+        generateCardPublisher(publisher)
+}
---
+++ frontend/js/script_publisher.js
@@ -0,0 +1,19 @@
+import * as controllerPublish from "./crud/crud_publisher.js"
+
+import { generateCardPublisher } from "./generate.js"
+import { loadingPublisher } from "./loadings.js"
+
+const form_publish_edit = document.querySelector("#form_publisher_edit");
+form_publish_edit.addEventListener("submit", async (event) => {
+    event.preventDefault()
+
+    try {
+        const objUser = Object.fromEntries(new FormData(form_publish_edit));
+        const publisher = await controllerPublish.create(objUser)
+        await generateCardPublisher(publisher);
+    } catch (error) {
+        alert(error.message)
+    }
+});
+
+loadingPublisher()
